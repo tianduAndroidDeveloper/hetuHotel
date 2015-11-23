c936bde4940a036c29fdf13e07092212016b6426
lushuzhen <1135741892@qq.com>
2015-11-23 08:35:45 +0800

    2015.11.23

--- kingtopgroup/src/com/kingtopgroup/activty/messageActivty.java
+++ kingtopgroup/src/com/kingtopgroup/activty/messageActivty.java
@@ -2,34 +2,43 @@ package com.kingtopgroup.activty;
 
 import net.tsz.afinal.FinalBitmap;
 
+import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.kingtogroup.location.DistanceUtils;
-import com.kingtogroup.location.LastLocation;
-import com.kingtogroup.messager.DiscusActivity;
-import com.kingtopgroup.R;
-import com.makeramen.RoundedImageView;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class messageActivty extends Activity{
+import com.kingtogroup.location.DistanceUtils;
+import com.kingtogroup.location.LastLocation;
+import com.kingtogroup.messager.DiscusActivity;
+import com.kingtopgroup.R;
+import com.kingtopgroup.constant.ConstanceUtil;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.stevenhu.android.phone.utils.ACache;
+import com.stevenhu.android.phone.utils.AsyncHttpCilentUtil;
+
+public class messageActivty extends Activity {
+	private static final String TAG = "messageActivty";
 	private ListView mListView;
 	private Context mContext;
-	
+	private ACache acache;
+	TextView discusCount;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -37,16 +46,18 @@ public class messageActivty extends Activity{
 		setContentView(R.layout.messagelistview);
 		mContext = this;
 		mListView = (ListView) findViewById(R.id.listView);
-		Adapter adapter = new Adapter(mContext,getIntent().getStringExtra("json"));
+		Adapter adapter = new Adapter(mContext, getIntent().getStringExtra(
+				"json"));
 		mListView.setAdapter(adapter);
 	}
 
 }
 
-class Adapter extends BaseAdapter{
+class Adapter extends BaseAdapter {
 	JSONArray dataList;
 	Context mContext;
-	public Adapter(Context context,String json){
+
+	public Adapter(Context context, String json) {
 		this.mContext = context;
 		try {
 			dataList = new JSONArray(json);
@@ -58,8 +69,8 @@ class Adapter extends BaseAdapter{
 
 	@Override
 	public int getCount() {
-		
-		return dataList==null?0:dataList.length();
+
+		return dataList == null ? 0 : dataList.length();
 	}
 
 	@Override
@@ -77,66 +88,151 @@ class Adapter extends BaseAdapter{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHolder holder;
-		if(convertView == null){
-			convertView = LayoutInflater.from(mContext).inflate(R.layout.messager_info_item, null);
+		if (convertView == null) {
+			convertView = LayoutInflater.from(mContext).inflate(
+					R.layout.messager_info_item, null);
 			holder = new ViewHolder();
 			holder.initwidget(convertView);
 			convertView.setTag(holder);
-		}else{
+		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
 		holder.initData(dataList.optJSONObject(position));
 		return convertView;
 	}
-	
-	class ViewHolder{
-		TextView nameSex,metter,discus,location,introduce,level;
+
+	class ViewHolder {
+		TextView nameSex, metter, discus, location, introduce, level,discusCount;
 		View discusLayout;
-		RoundedImageView headPhoto;
-		FinalBitmap finalBitMap=FinalBitmap.create(mContext);
-		public void initwidget(View view){
+		ImageView headPhoto;
+		FinalBitmap finalBitMap = FinalBitmap.create(mContext);
+
+		public void initwidget(View view) {
 			this.nameSex = (TextView) view.findViewById(R.id.nameSex);
 			this.metter = (TextView) view.findViewById(R.id.metter);
 			this.discus = (TextView) view.findViewById(R.id.discusCount);
 			this.location = (TextView) view.findViewById(R.id.location);
 			this.introduce = (TextView) view.findViewById(R.id.introduce);
 			this.level = (TextView) view.findViewById(R.id.level);
-			this.headPhoto = (RoundedImageView) view.findViewById(R.id.headPhoto);
+			this.headPhoto = (ImageView) view
+					.findViewById(R.id.headPhoto);
 			this.discusLayout = view.findViewById(R.id.discusLayout);
+			
+			this.discusCount=(TextView) view.findViewById(R.id.discusCount);
+
 			this.discusLayout.setOnClickListener(new OnClickListener() {
-				
+				private ACache acache;
+
 				@Override
 				public void onClick(View v) {
 					JSONObject obj = (JSONObject) v.getTag();
-					Intent intent = new Intent(mContext,DiscusActivity.class);
+					Intent intent = new Intent(mContext, DiscusActivity.class);
 					intent.putExtra("json", obj.toString());
+					acache = ACache.get(mContext);
+					acache.put("discus_json", obj.toString());
 					mContext.startActivity(intent);
 				}
 			});
+
+			// 点击头像
+			this.headPhoto.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					JSONObject obj = (JSONObject) v.getTag();
+					Intent inten = new Intent(mContext,
+							MessagerDetialActivty.class);
+					inten.putExtra("json", obj.toString());
+					//inten.putExtra("discount_num",v.getTag(12).toString());
+					mContext.startActivity(inten);
+
+				}
+			});
+
 		}
-		
-		public void initData(JSONObject obj){
-			if(obj == null)
-				return;
-			this.nameSex.setText(obj.optString("name").trim() + "  " + (obj.optInt("sex")==0?"女":"男"));
-			try{
-			double lat = obj.optDouble("point_x", 0);
-			double lgn = obj.optDouble("point_y", 0);
+
+		public void initData(final JSONObject obj) {
 			
-			double distance = DistanceUtils.GetDistance(lat, lgn, LastLocation.initInstance().getLatitude(), LastLocation.initInstance().getLongitude());
-			this.metter.setText(String.valueOf(distance));
-			}catch(Exception e){
+			if (obj == null)
+				return;
+			this.nameSex.setText(obj.optString("name").trim() + "  "
+					+ (obj.optInt("sex") == 0 ? "女" : "男"));
+			try {
+				double lat = obj.optDouble("point_x", 0);
+				double lgn = obj.optDouble("point_y", 0);
+
+				double distance = DistanceUtils.GetDistance(lat, lgn,
+						LastLocation.initInstance().getLatitude(), LastLocation
+								.initInstance().getLongitude());
+				this.metter.setText(String.valueOf(distance));
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+             
 			this.discus.setText(obj.optString("announcement").trim());
 			this.location.setText(obj.optString("regionName").trim());
 			this.introduce.setText(obj.optString("description").trim());
 			this.level.setText(obj.optString("title").trim());
+			String uri = "http://kingtopgroup.com/upload/store/" + obj.optString("storeid")
+					+ "/logo/thumb150_150/" + obj.optString("logo");
+			Log.i("messageActivty", uri);
+			ImageLoader.getInstance().displayImage(uri.trim(), this.headPhoto);
 			
-			finalBitMap.display(this.headPhoto, mContext.getString(R.string.url_imgHost) + obj.optString("logo").trim(),BitmapFactory.decodeResource(mContext.getResources(), R.drawable.photos));
+			//获取评价数
+			String storeid=obj.optString("storeid");
+			
+			
+			RequestParams params=AsyncHttpCilentUtil.getParams();
+			params.put("massagerid", storeid);
+			AsyncHttpCilentUtil.getInstance().get(ConstanceUtil.get_masserger_list_url, params, new AsyncHttpResponseHandler() {
+				@Override
+				public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
+					if(arg0==200){
+						String date =new String(arg2);
+						JSONObject obj2;
+						try {
+							obj2 = new JSONObject(date);
+							String ReviewTotalList=obj2.getString("ReviewTotalList");
+				        	JSONArray array=new JSONArray(ReviewTotalList);
+				        	discusCount.setText(array.length()+"");
+				        	obj.put("discusCount", array.length()+"");
+				        	//headPhoto.setTag(12, array.length());
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						
+						//try {
+							//JSONObject obj=new JSONObject(date);
+							//String ReviewTotalList=obj.getString("ReviewTotalList");
+							//initViewPager(date);
+						//} catch (JSONException e) {
+						//	e.printStackTrace();
+						//}
+					}
+				}
+				
+				@Override
+				public void onFailure(int arg0, Header[] arg1, byte[] arg2, Throwable arg3) {
+					
+				}
+			});
+			
+		/*} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}*/
+    	
+		//}
+    
+   // }
+			
 			this.discusLayout.setTag(obj);
+			this.headPhoto.setTag(obj);
+			
+
 		}
 	}
-	
+
 }
